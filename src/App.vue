a2586506d205c0fe302fcc336a58c5c07991d67d
ghm83 <obuhov.gh@gmail.com>
2020-06-04 14:12:14 +0300

    font awesome

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>asdfasd
+      <i class="fa fa-address-book" aria-hidden="true"></i>
+  </h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "./styles/variables";
+
+h1 {
+  color: $blue;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+import Vue from 'vue'
+import App from './App'
 import './styles/index.scss'
 
-window.Vue = require('vue')
-
-Vue.component('example-component',require('./components/example.vue').default)
-
-const app = new Vue({
-    el: '#app'
+new Vue({
+    el: '#app',
+    render: h => h(App)
 })
 
+// window.Vue = require('vue')
 
-console.log('1111')
+// Vue.component('example-component',require('./components/example').default)
+
+// const app = new Vue({
+//     el: '#app'
+// })
